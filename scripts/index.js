ebaf77cc94679244f677fa625f8c23e8b087614b
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-06-26 12:08:02 +0300

    mob menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,36 @@
+
+
+document.addEventListener("DOMContentLoaded", bootstrap);
+
+
 function bootstrap() {
+
+  const burgerIconMenu = document.querySelector('.header__burger-icon');
+  const closeIcon = document.querySelector('.header__close-icon');
+  
+
+  const toggleMenu = (evt) => {
+    const menu = document.querySelector('.mob-menu');
+    menu.classList.toggle('mob-menu_visible');
+
+    burgerIconMenu.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+
+  }
+
+
 
   const elYear = document.querySelector('.footer__year');
   if (elYear) {
     elYear.innerHTML = new Date().getFullYear();
   }
+
+  if (burgerIconMenu && closeIcon) {
+    burgerIconMenu.addEventListener('click', toggleMenu);
+    closeIcon.addEventListener('click', toggleMenu);
+  }
+
+
   
 }
 
-
-document.addEventListener("DOMContentLoaded", bootstrap);
